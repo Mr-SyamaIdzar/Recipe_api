04c34a1d2e5b8d1df9b20e0547ff0edb21ecc896
Mr. Syamaidzar <muhammadraihansyamaidzar@gmail.com>
2025-02-22 15:50:52 +0700

    Javascript Coding

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+const recipe = document.getElementById("recipeContainer");
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("Btn Clicked");
+});
