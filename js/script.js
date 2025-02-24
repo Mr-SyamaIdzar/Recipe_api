330b7828da8796db71bdb8c9a49d6dedab7333e7
Mr. Syamaidzar <muhammadraihansyamaidzar@gmail.com>
2025-02-24 21:28:17 +0700

    Done?

--- js/script.js
+++ js/script.js
@@ -6,32 +6,36 @@ const recipeCloseBtn = document.querySelector(".recipe-close-btn");
 
 const fetchRecipes = async (query) => {
   recipe.innerHTML = `<h2>Fetching Recipes...</h2>`;
-  const data = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
-  );
-  const response = await data.json();
+  try {
+    const data = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+    );
+    const response = await data.json();
 
-  recipe.innerHTML = "";
-  response.meals.forEach((meal) => {
-    const recipeDiv = document.createElement("div");
-    recipeDiv.classList.add("recipe");
-    recipeDiv.innerHTML = `
-      <img src="${meal.strMealThumb}">
-      <h3>${meal.strMeal}</h3>
-      <p><span>${meal.strArea}</span> Dish</p>
-      <p>Belongs to <span>${meal.strCategory}</span> Category</p>
-    `;
+    recipe.innerHTML = "";
+    response.meals.forEach((meal) => {
+      const recipeDiv = document.createElement("div");
+      recipeDiv.classList.add("recipe");
+      recipeDiv.innerHTML = `
+        <img src="${meal.strMealThumb}">
+        <h3>${meal.strMeal}</h3>
+        <p><span>${meal.strArea}</span> Dish</p>
+        <p>Belongs to <span>${meal.strCategory}</span> Category</p>
+      `;
 
-    const btn = document.createElement("button");
-    btn.textContent = "View Recipe";
-    recipeDiv.appendChild(btn);
+      const btn = document.createElement("button");
+      btn.textContent = "View Recipe";
+      recipeDiv.appendChild(btn);
 
-    btn.addEventListener("click", () => {
-      openRecipePopup(meal);
+      btn.addEventListener("click", () => {
+        openRecipePopup(meal);
+      });
+
+      recipe.appendChild(recipeDiv);
     });
-
-    recipe.appendChild(recipeDiv);
-  });
+  } catch (error) {
+    recipe.innerHTML = "<h2>Error in Fetching Recipes...</h2>";
+  }
 };
 
 const fetchIngredients = (meal) => {
@@ -51,24 +55,29 @@ const fetchIngredients = (meal) => {
 
 const openRecipePopup = (meal) => {
   recipeDetailsContent.innerHTML = `
-    <h2 class="recipeNmae">${meal.strMeal}</h2>
+    <h2 class="recipeName">${meal.strMeal}</h2>
     <h3>Ingredients:</h3>
     <ul class="ingredientList">${fetchIngredients(meal)}</ul>
-    <div>
+    <div class="recipeInstructions">
       <h3>Instructions:</h3>
-      <p class="recipeInstructions">${meal.strInstructions}</p>
+      <p>${meal.strInstructions}</p>
     </div>
   `;
 
-  
   recipeDetailsContent.parentElement.style.display = "block";
 };
 
 recipeCloseBtn.addEventListener("click", () => {
   recipeDetailsContent.parentElement.style.display = "none";
-})
+});
+
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
   const searchValue = searchInput.value.trim();
+
+  if (!searchValue) {
+    recipe.innerHTML = `<h2>Type the meal in the search box</h2>`;
+    return;
+  }
   fetchRecipes(searchValue);
 });
